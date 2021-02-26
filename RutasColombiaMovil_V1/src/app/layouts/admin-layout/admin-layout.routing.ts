d586df0df63638fef304fa1b447725d0c6a0c520
cmrestrepo <37378528+cmrestrepo@users.noreply.github.com>
2021-02-26 09:08:43 -0500

    borrar

--- RutasColombiaMovil_V1/src/app/layouts/admin-layout/admin-layout.routing.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-
-import { UserProfileComponent } from '../../user-profile/user-profile.component';
-import { MapsComponent } from '../../maps/maps.component';
-
-export const AdminLayoutRoutes: Routes = [        
-    { path: 'user-profile',   component: UserProfileComponent },
-    { path: 'maps',           component: MapsComponent }
-];
